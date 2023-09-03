9116a24bde105650ff75c38059852de1b26162cf
Lucas Abbade <lucasabbade@hotmail.com>
2023-09-03 09:49:48 -0300

    finish env data setup

--- portfolio_rl/gym_env/gym_env.py
+++ portfolio_rl/gym_env/gym_env.py
@@ -1,6 +1,5 @@
 from typing import Optional, Dict, Any, Tuple, SupportsFloat
 
-import gymnasium as gym
 from gymnasium import Env
 from gymnasium.spaces import Space
 from gymnasium.core import ObsType, ActType
@@ -25,7 +24,31 @@ class PortfolioEnv(Env):
         seed: Optional[int] = None,
         options: Dict[str, Any] = {}
     ) -> tuple[ObsType, Dict[str, Any]]:
-        """Resets the environment to an initial internal state, returning an initial observation and info"""
+        """
+        Resets the environment to an initial internal state, returning an initial observation and info.
+
+        This method generates a new starting state often with some randomness to ensure that the agent explores the
+        state space and learns a generalised policy about the environment. This randomness can be controlled
+        with the ``seed`` parameter otherwise if the environment already has a random number generator and
+        :meth:`reset` is called with ``seed=None``, the RNG is not reset.
+
+        Args:
+            seed (optional int): The seed that is used to initialize the environment's PRNG (`np_random`).
+                If the environment does not already have a PRNG and ``seed=None`` (the default option) is passed,
+                a seed will be chosen from some source of entropy (e.g. timestamp or /dev/urandom).
+                However, if the environment already has a PRNG and ``seed=None`` is passed, the PRNG will *not* be reset.
+                If you pass an integer, the PRNG will be reset even if it already exists.
+                Usually, you want to pass an integer *right after the environment has been initialized and then never again*.
+                Please refer to the minimal example above to see this paradigm in action.
+            options (optional dict):
+                - wallet: wallet size in float (default: 1,000,000)
+
+        Returns:
+            observation (ObsType): Observation of the initial state. This will be an element of :attr:`observation_space`
+                (typically a numpy array) and is analogous to the observation returned by :meth:`step`.
+            info (dictionary):  This dictionary contains auxiliary information complementing ``observation``. It should be analogous to
+                the ``info`` returned by :meth:`step`.
+        """
         super().reset(seed=seed)
         if 'wallet' in options:
             self.wallet = options['wallet']
@@ -33,5 +56,25 @@ class PortfolioEnv(Env):
     def step(
         self, action: ActType
     ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
-        """Run one timestep of the environment's dynamics using the agent actions."""
+        """Run one timestep of the environment's dynamics using the agent actions.
+
+        When the end of an episode is reached (``terminated or truncated``), it is necessary to call :meth:`reset` to
+        reset this environment's state for the next episode.
+
+        Args:
+            action (ActType): an action provided by the agent to update the environment state.
+
+        Returns:
+            observation (ObsType): An element of the environment's :attr:`observation_space` as the next observation due to the agent actions.
+                An example is a numpy array containing the positions and velocities of the pole in CartPole.
+            reward (SupportsFloat): The reward as a result of taking the action.
+            terminated (bool): Whether the agent reaches the terminal state (as defined under the MDP of the task)
+                which can be positive or negative. An example is reaching the goal state or moving into the lava from
+                the Sutton and Barton, Gridworld. If true, the user needs to call :meth:`reset`.
+            truncated (bool): Whether the truncation condition outside the scope of the MDP is satisfied.
+                Typically, this is a timelimit, but could also be used to indicate an agent physically going out of bounds.
+                Can be used to end the episode prematurely before a terminal state is reached.
+                If true, the user needs to call :meth:`reset`.
+            info (dict): Contains auxiliary diagnostic information (helpful for debugging, learning, and logging)
+        """
         pass
